0456002136bcf54566bea50c890575e339a8861a
adityyakaushal@gmail.com <adityyakaushal@gmail.com>
2026-02-26 16:10:07 -0500

    added driver to motor controller task

--- Tasks/Src/motor_controller.cpp
+++ Tasks/Src/motor_controller.cpp
@@ -1,4 +1,8 @@
 #include "motor_controller.hpp"
+#include "odrives1.hpp"
+#include "stm32H7xx_hal_fdcan.h"
+
+extern FDCAN_HandleTypeDef hfdcan1;
 
 osThreadId_t motorControllerTaskHandle;
 
@@ -10,10 +14,14 @@ static const osThreadAttr_t motorController_attributes = {
 
 void motorControllerMainLoop(void *arg)
 {
+  
+  ODRIVES1 odrive(&hfdcan1);
+  odrive_can_heartbeat_t heartbeat = {0};
 
   while(true)
   {
     BSP_LED_Toggle(LED_GREEN);
+    odrive.getHeartbeat(&heartbeat);
     osDelay(1000);
   }
 }
