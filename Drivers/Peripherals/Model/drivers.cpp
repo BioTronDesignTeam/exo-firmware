3813661dc9ff927abf19b60ac78c13865f7b5d37
Frank Zeng <frankhzeng@gmail.com>
2026-07-17 17:55:32 -0400

    port code from testing repo

--- Drivers/Peripherals/Model/drivers.cpp
+++ Drivers/Peripherals/Model/drivers.cpp
@@ -1,18 +1,22 @@
 #include "drivers.hpp"
 // Credit: WARG efs-zeropilot for code structure!
-#include <new>
+
 // All the hardware handles
 extern FDCAN_HandleTypeDef hfdcan1;
+extern I2C_HandleTypeDef hi2c1;
 
 // Following best practices, we statically allocate memory for each driver class
 // Note that we just put the class name in the brackets as alignas(className) is
 // equivalent to alignas(alignof(className)) which aligns everything correctly!
 alignas(ODRIVES1) static uint8_t odrives1Storage[sizeof(ODRIVES1)];
+alignas(BNO085) static uint8_t bno085Storage[sizeof(BNO085)];
 
 // Now we create global handles that we can use anywhere!
 ODRIVES1 *odriveS1Handle = nullptr;
+BNO085 *bno085Handle = nullptr;
 
 // Driver Initialization
-void initializeDrivers() {
+void initDrivers() {
 	odriveS1Handle = new (&odrives1Storage) ODRIVES1(&hfdcan1);
+	bno085Handle = new (&bno085Storage) BNO085(&hi2c1);
 }
--- Drivers/Peripherals/Model/drivers.hpp
+++ Drivers/Peripherals/Model/drivers.hpp
@@ -1,7 +1,9 @@
 #pragma once
-
+#include <new>
 #include "odriveS1.hpp"
+#include "BNO085.hpp"
 
 extern ODRIVES1 *odriveS1Handle;
+extern BNO085 *bno085Handle;
 
-void initializeDrivers();
+void initDrivers();
