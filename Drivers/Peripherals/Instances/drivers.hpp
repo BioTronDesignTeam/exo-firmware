8126f58a82e6a0fb621bec3ba7286715926ae781
Frank Zeng <frankhzeng@gmail.com>
2026-07-18 13:48:05 -0400

    architecture changes

---
+++ Drivers/Peripherals/Instances/drivers.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "odriveS1.hpp"
+
+extern ODRIVES1 *odriveS1Handle;
+
+void initializeDrivers();
--- Drivers/Peripherals/Model/drivers.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "odriveS1.hpp"
-
-extern ODRIVES1 *odriveS1Handle;
-
-void initializeDrivers();
