dfe82f8b2a533ef624f81dbaff72d02ed8bafe6f
Frank Zeng <frankhzeng@gmail.com>
2026-07-18 13:48:05 -0400

    architecture changes

--- Drivers/Peripherals/Instances/drivers.hpp
+++ Drivers/Peripherals/Instances/drivers.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
 #include "odriveS1.hpp"
-#include "mpu6050.hpp"
-#include "bno085.hpp"
 
 extern ODRIVES1 *odriveS1Handle;
-extern MPU6050 *MPU6050Handle;
-extern BNO085 *bno085Handle;
 
 void initializeDrivers();
